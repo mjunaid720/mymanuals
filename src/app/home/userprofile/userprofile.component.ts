f5c2509357fea3bab78239cef995d4097018a21b
Philipp K <sk223if@student.lnu.se>
2018-12-12 19:00:59 +0100

    Added subscription button to the user profile page 	modified:   src/app/home/userprofile/userprofile.component.html 	modified:   src/app/home/userprofile/userprofile.component.ts

--- src/app/home/userprofile/userprofile.component.ts
+++ src/app/home/userprofile/userprofile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-userprofile',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserprofileComponent implements OnInit {
 
-  constructor() { }
+  isSubscribed: any;
+  userProfileData: any;
+
+  constructor(public http:HttpClient) { }
+
 
   ngOnInit() {
+    this.getUserProfileData();
   }
 
+  getUserProfileData(){
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': JSON.parse(localStorage["data"]).token
+      })
+    }
+    this.http.get("http://localhost:8080/api/consumer/profile",  httpOptions)
+      .subscribe((data: any) => this.userProfileData ={
+        subscribed: data['subscription']
+      })
+    var that = this;
+    setInterval(function () {
+      that.isSubscribed = that.userProfileData.subscribed;
+    },1000);
+  }
+
+
+  subscribed() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': JSON.parse(localStorage["data"]).token
+      })
+    }
+    if (this.isSubscribed === true) {
+      this.http.put("http://localhost:8080/api/consumer/subscription",{"subscription": true}, httpOptions).subscribe();
+      this.userProfileData.subscribed = true;
+    } else {
+      this.http.put("http://localhost:8080/api/consumer/subscription",{"subscription": false}, httpOptions).subscribe();
+      this.userProfileData.subscribed = false;
+    }
+  }
 }
